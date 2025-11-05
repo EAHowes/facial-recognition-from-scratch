087d0c9e7ce4d30137b2dbc42b8ac9ec404ac5cc
Ethan <ethanhowes06@gmail.com>
2025-11-04 23:01:47 -0500

    new values to be tested on desktop

--- src/dbscan_wrapper.py
+++ src/dbscan_wrapper.py
@@ -22,54 +22,8 @@ if __name__ == "__main__":
 
     # initialize eps testing range
     # for eps in [1.001, 1.002, 1.003, 1.004]:
-    for minPts in [8, 9]:
 
-        t0 = time.time()
-        labels_out = dbscan(EMBEDDINGS_ARRAY, EPS, minPts)
-        time_passed = time.time() - t0
-
-        predicted_clustering = np.asarray(labels_out, dtype=int)
-
-        points = len(predicted_clustering)
-        noise_points = int(np.sum(predicted_clustering == -1))
-        cluster_ids = [c for c in np.unique(predicted_clustering) if c >= 1]
-        clusters = len(cluster_ids)
-
-        print("\ndbscan results")
-        print(f"eps: {EPS}")
-        print(f"minPts: {minPts}")
-        print(f"runtime: {time_passed:.2f} s")
-        print(f"points: {points}")
-        print(f"clusters: {clusters}")
-        print(f"noise points: {noise_points}")
-
-        # compare the predicted results from home-made dbscan to the ground truth labels
-        GROUND_TRUTH = np.load(Path("../data/labels.npy"))
-
-        # make ground truth size the same as embedding size
-        GROUND_TRUTH = GROUND_TRUTH[:EMBEDDINGS_ARRAY.shape[0]]
-        assert len(labels_out) == len(GROUND_TRUTH)
-
-        # ignore all noise points
-        mask = predicted_clustering != -1
-        predicted_clustering = predicted_clustering[mask]
-        gt = GROUND_TRUTH[mask]
-
-        if len(predicted_clustering) == 0:
-            print("no points left. all points were labeled as noise.")
-        else:
-            total = len(predicted_clustering)   # find total numbers of non noise points
-            correct = 0
-            for cluster_id in np.unique(predicted_clustering):      # compute the correctness of each point found in cluster
-                idx = (predicted_clustering == cluster_id)
-                labels, counts = np.unique(gt[idx], return_counts=True)
-                correct += counts.max()
-            purity = correct / total
-            print(f"Purity: {purity:.4f}  (based on {total} non-noise points)")
-
-############################################
-
-    for eps in [1.001, 1.002, 1.003, 1.005, 1.005, 1.006, 1.007]:
+    for eps in [1.006, 1.007, 1.008, 1.009]:
 
         t0 = time.time()
         labels_out = dbscan(EMBEDDINGS_ARRAY, eps, MINPTS)
